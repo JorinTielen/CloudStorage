9544817c3f2a96f68c7e81aeb04363bb09fbe2cb
Jorin Tielen <jorintielen999@gmail.com>
2018-01-09 14:53:05 +0100

    Improved downloading of files
    - Now the file extension will allways be correct.

--- src/cloudstorage/client/FileDownloader.java
+++ src/cloudstorage/client/FileDownloader.java
@@ -6,8 +6,14 @@ import java.io.FileWriter;
 
 public class FileDownloader {
     public static void download(String path, File file) {
+        String filename = file.getName();
+        String[] pieces = filename.split("[.]");
+        if (!pieces[pieces.length - 1].equals("txt")) {
+            filename += ".txt";
+        }
+
         try (BufferedWriter writer =
-                     new BufferedWriter(new FileWriter(path + file.getName()))) {
+                     new BufferedWriter(new FileWriter(path + filename))) {
             writer.write(file.getText());
         } catch (Exception e) {
             e.printStackTrace();
